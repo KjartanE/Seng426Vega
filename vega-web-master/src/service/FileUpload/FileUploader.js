6dc1cb4f51ef0057c7277a09636cb033af63a73e
KjartanE <kjartanreinarsson@gmail.com>
2022-07-01 12:06:43 -0700

    change env

--- vega-web-master/src/service/FileUpload/FileUploader.js
+++ vega-web-master/src/service/FileUpload/FileUploader.js
@@ -4,14 +4,14 @@ const API_URL = process.env.API_URL;
 
 export function fileUploader(fileInfo, token){
 	console.log("In File Uploader", token);
-	return doPostFile("http://${API_URL}/api/venus/upload", fileInfo, token);
+	return doPostFile(`http://${API_URL}/api/venus/upload`, fileInfo, token);
 }
 
 export function fetchFiles(token){
 	console.log("fetchFiles", token);
-	return doGet("http://${API_URL}/api/venus/listfiles", token)
+	return doGet(`http://${API_URL}/api/venus/listfiles`, token)
 }
 
 export function fetchData(name, token){
-	return doGet("http://${API_URL}/api/venus/fetchcontent?name="+name, token)
+	return doGet(`http://${API_URL}/api/venus/fetchcontent?name=`+name, token)
 }
